9a985fb12349b608cb1cb806108f220aedd5135f
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-05-04 08:26:55 +0600

    fixed nav bar responsive issue in mobile devices

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
     return (
         <div>
             <Header />
+
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/home" element={<Home />} />
--- src/Shared/Header.js
+++ src/Shared/Header.js
@@ -2,13 +2,14 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { signOut } from 'firebase/auth';
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import auth from '../firebase.init';
 import ActiveLink from '../hooks/ActiveLink';
 
 function Header() {
+    const [open, setOpen] = useState(false);
     const navigate = useNavigate();
     const [user, loading, error] = useAuthState(auth);
 
@@ -17,13 +18,20 @@ function Header() {
     };
 
     return (
-        <nav className=" rounded border-gray-200 bg-sky-50 px-2 py-2.5 transition-all duration-200 dark:bg-gray-800 sm:px-4">
-            <div className="container mx-auto flex items-center justify-between">
-                <ActiveLink to="/" className="flex items-center">
-                    <span className="text-md self-center whitespace-nowrap font-semibold text-gray-700 dark:text-white lg:text-xl">
-                        Pronex-Wood
-                    </span>
-                </ActiveLink>
+        <nav className=" rounded border-gray-200 bg-gray-100 px-2 py-2.5 transition-all duration-200 dark:bg-gray-800 sm:px-4">
+            <ActiveLink to="/" className=" block w-28 translate-y-8 md:hidden">
+                <span className="text-md self-center whitespace-nowrap font-semibold text-gray-700 dark:text-white lg:text-xl">
+                    Pronex-Wood
+                </span>
+            </ActiveLink>
+            <div className="container mx-auto flex flex-col items-end md:flex-row md:items-center md:justify-between">
+                <div className="hidden md:block">
+                    <ActiveLink to="/" className="flex items-center">
+                        <span className="text-md self-center whitespace-nowrap font-semibold text-gray-700 dark:text-white lg:text-xl">
+                            Pronex-Wood
+                        </span>
+                    </ActiveLink>
+                </div>
                 <div className="flex items-center md:order-2">
                     <div className="hidden md:block">
                         <div className="flex items-center">
@@ -49,27 +57,29 @@ function Header() {
                         </div>
                     </div>
                     <button
+                        onClick={() => setOpen(!open)}
                         data-collapse-toggle="mobile-menu-4"
                         type="button"
-                        className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+                        className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-sky-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden "
                         aria-controls="mobile-menu-4"
                         aria-expanded="false"
                     >
                         <span className="sr-only">Open main menu</span>
                         <svg
-                            className="h-6 w-6"
+                            className={` ${open ? 'hidden' : ''} h-6 w-6`}
                             fill="currentColor"
                             viewBox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg"
                         >
                             <path
+                                x-show="!open"
                                 fillRule="evenodd"
-                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
                                 clipRule="evenodd"
                             />
                         </svg>
                         <svg
-                            className="hidden h-6 w-6"
+                            className={` ${open ? '' : 'hidden'} h-6 w-6`}
                             fill="currentColor"
                             viewBox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg"
@@ -94,7 +104,7 @@ function Header() {
                         </li>
                         <li>
                             <ActiveLink to="/inventory" className="nav-link">
-                                Manage Inventory
+                                Inventory
                             </ActiveLink>
                         </li>
                         <li>
@@ -108,6 +118,27 @@ function Header() {
                             </ActiveLink>
                         </li>
                     </ul>
+                    <div className="flex items-center md:hidden">
+                        <ul className=" flex flex-col items-center">
+                            <li>
+                                <ActiveLink to="/addItem" className="nav-link">
+                                    Add item
+                                </ActiveLink>
+                            </li>
+                            <li>
+                                <ActiveLink to="/myItems" className="nav-link">
+                                    My items
+                                </ActiveLink>
+                            </li>
+                        </ul>
+                        <button
+                            onClick={handleLoginLogOutButton}
+                            type="button"
+                            className="mr-3 hidden rounded bg-sky-700 px-5 py-1.5 text-center text-sm font-medium text-white shadow-lg shadow-gray-300 hover:bg-sky-800 focus:outline-none focus:ring-4 focus:ring-sky-300 dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800 md:mr-0 md:block"
+                        >
+                            {user ? 'Log out' : 'Log in'}
+                        </button>
+                    </div>
                     <button
                         onClick={handleLoginLogOutButton}
                         type="button"
